09cef753cfdbbf657420007581a0ba4e8b1b78df
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-03-14 12:40:49 -0400

    initialized all files and began adding data

---
+++ data.js
@@ -0,0 +1,31 @@
+var movies = [
+  {
+    title: "Snatch",
+    desc: "Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers, and supposedly Jewish jewelers fight to track down a priceless stolen diamond.",
+    director: "Guy Ritchie",
+    stars: "Jason Statham, Brad Pitt, Benicio Del Toro",
+    img: "images/snatch.jpg"
+  },
+  {
+    title: "Ace Ventura",
+    desc: "Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers, and supposedly Jewish jewelers fight to track down a priceless stolen diamond.",
+    director: "Guy Ritchie",
+    stars: "Jason Statham, Brad Pitt, Benicio Del Toro",
+    img: "images/snatch.jpg"
+  },
+  {
+    title: "Reservoir Dogs",
+    desc: "Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers, and supposedly Jewish jewelers fight to track down a priceless stolen diamond.",
+    director: "Guy Ritchie",
+    stars: "Jason Statham, Brad Pitt, Benicio Del Toro",
+    img: "images/snatch.jpg"
+  },
+  {
+    title: "Monty Python and the Holy Grail",
+    desc: "Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers, and supposedly Jewish jewelers fight to track down a priceless stolen diamond.",
+    director: "Guy Ritchie",
+    stars: "Jason Statham, Brad Pitt, Benicio Del Toro",
+    img: "images/snatch.jpg"
+  },
+
+];
